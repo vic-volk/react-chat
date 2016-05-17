3e9bdd48d2ac553800ede2a72fdf7ba27cb25375
Andrey Volkov <vic-volk@yandex.ru>
2016-05-17 17:11:57 +0400

    chat completion

--- js/example.js
+++ js/example.js
@@ -3,16 +3,48 @@ var ChatBox = React.createClass({
         return (
             <div className="chatBox">
                 <h2>Chat box</h2>
-                <ChatTextBox chatModel={this.props.chatModel}/>
+                <ChatTextBox chatModel={this.props.chatModel} chatList={this.props.chatList}/>
             </div>
         );
     }
 });
 
+var MessageList = React.createClass({
+    render: function() {
+        var messagesNodes = this.props.data.map(function (message) {
+            console.log(message);
+            return (
+                <Message author={message.author} key={message.id}>
+                    {message.text}
+                </Message>
+            );
+        });
+        console.log(messagesNodes);
+        return (
+            <div class="messageList">
+                {messagesNodes}
+            </div>
+        );
+    }
+});
+
+var Message = React.createClass({
+  render: function() {
+    return (
+      <div className="message">
+        <h2 className="messageAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
 var ChatTextBox = React.createClass({
     getInitialState: function() {
-      return {value: this.props.chatModel.text};
       console.log('init-state');
+      return {value: this.props.chatModel.text, index: this.props.chatList[0].id + 1};
     },
     handleChange: function(event) {
       this.setState({value: event.target.value});
@@ -20,10 +52,14 @@ var ChatTextBox = React.createClass({
     },
     handleKeyPress: function(event) {
       if (event.charCode == 13) {
-          this.setState({value: event.target.value});
+          this.setState({value: event.target.value, index: this.state.index + 1});
           this.props.chatModel.text = event.target.value;
+          var message = { id: this.state.index, author: "test", text: this.props.chatModel.text};
+          this.props.chatList.push(message);
           console.log('key-press');
           console.log('event.target.value: ' + event.target.value);
+          console.log('chat-list:' + this.props.chatList)
+          this.forceUpdate();
       }
     },
     render: function() {
@@ -37,6 +73,7 @@ var ChatTextBox = React.createClass({
                     onKeyPress={this.handleKeyPress}
                 /><br/>
                 <label>{this.props.chatModel.text}</label>
+                <MessageList data={this.props.chatList}/>
             </div>
         );
     }
@@ -46,7 +83,11 @@ var chatModel = {
     text: "text-124"
 };
 
+var chatList = [
+    { id: "1", author: "test", text: "test-text" }
+]
+
 ReactDOM.render(
-  <ChatBox chatModel={chatModel}/>,
+  <ChatBox chatModel={chatModel} chatList={chatList}/>,
   document.getElementById('chat-box')
 );
