1679f1c2c2145f1ee8db4c5437615c90494ea815
Andrey Volkov <vic-volk@yandex.ru>
2016-05-18 19:44:23 +0400

    labels and reactive state

--- js/example.js
+++ js/example.js
@@ -3,7 +3,7 @@ var ChatBox = React.createClass({
         return (
             <div className="chatBox">
                 <h2>Chat box</h2>
-                <ChatTextBox chatModel={this.props.chatModel} chatList={this.props.chatList}/>
+                <ChatTextBox chatModel={this.props.chatModel} chatList={this.props.chatList} serverMessages={serverMessages}/>
             </div>
         );
     }
@@ -14,7 +14,7 @@ var MessageList = React.createClass({
         var messagesNodes = this.props.data.map(function (message) {
             console.log(message);
             return (
-                <Message author={message.author} key={message.id}>
+                <Message author={message.author} key={message.id} date={message.date}>
                     {message.text}
                 </Message>
             );
@@ -31,12 +31,13 @@ var MessageList = React.createClass({
 var Message = React.createClass({
   render: function() {
     return (
-      <div className="message">
-        <h2 className="messageAuthor">
-          {this.props.author}
-        </h2>
-        {this.props.children}
-      </div>
+        <div className="row">
+            <span className={(this.props.className || '') + ' label label-default'}>{this.props.author}</span>
+            <span className={(this.props.className || '') + ' label label-success'}>{this.props.date}</span>
+            <div className="message">
+                {this.props.children}
+            </div>
+        </div>
     );
   }
 });
@@ -44,7 +45,7 @@ var Message = React.createClass({
 var ChatTextBox = React.createClass({
     getInitialState: function() {
       console.log('init-state');
-      return {value: this.props.chatModel.text, index: this.props.chatList[0].id + 1};
+      return {value: this.props.chatModel.text, index: 1, serverMessageIndex: 1};
     },
     handleChange: function(event) {
       this.setState({value: event.target.value});
@@ -54,7 +55,7 @@ var ChatTextBox = React.createClass({
       if (event.charCode == 13) {
           this.setState({value: event.target.value, index: this.state.index + 1});
           this.props.chatModel.text = event.target.value;
-          var message = { id: this.state.index, author: "test", text: this.props.chatModel.text};
+          var message = { id: this.state.index, author: "you", text: this.props.chatModel.text, date: new Date() + ""};
           this.props.chatList.push(message);
           console.log('key-press');
           console.log('event.target.value: ' + event.target.value);
@@ -62,9 +63,20 @@ var ChatTextBox = React.createClass({
           this.forceUpdate();
       }
     },
+    componentDidMount: function() {
+        setInterval(this.requestAnswers, 10000); // Call a method on the mixin
+    },
+    requestAnswers: function() {
+        this.state.index = this.state.index + 1;
+        var index = this.state.index % 5 - 1;
+        this.props.chatList.push({ id: this.state.index, author: "agent", text: this.props.serverMessages[index], date: new Date() + ""});
+    },
     render: function() {
         return (
             <div className="chatTextBox">
+                <div className={(this.props.className || '') + 'row' style='overflow: scroll'}>
+                    <MessageList data={this.props.chatList}/>
+                </div>
                 <input 
                     type="textarea" 
                     placeholder="chat with cognitive agent."
@@ -72,22 +84,20 @@ var ChatTextBox = React.createClass({
                     onChange={this.handleChange}
                     onKeyPress={this.handleKeyPress}
                 /><br/>
-                <label>{this.props.chatModel.text}</label>
-                <MessageList data={this.props.chatList}/>
             </div>
         );
     }
 });
 
 var chatModel = {
-    text: "text-124"
+    text: ""
 };
 
-var chatList = [
-    { id: "1", author: "test", text: "test-text" }
-]
+var chatList = [];
+var serverMessages = ["Hi!", "Hello!", "Just a moment.", "Please, ask me smth.", "Yes, we have some books in this theme."];
 
-ReactDOM.render(
-  <ChatBox chatModel={chatModel} chatList={chatList}/>,
-  document.getElementById('chat-box')
-);
+setInterval(function() {
+    ReactDOM.render(
+        <ChatBox chatModel={chatModel} chatList={chatList} serverMessages={serverMessages}/>,
+    document.getElementById('chat-box')
+);}, 100);
